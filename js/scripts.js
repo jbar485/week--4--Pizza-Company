5992c076b8847dc64e434ab80293a01c501067d0
Judah Barton <judah.barton@yahoo.com>
2019-11-08 10:06:21 -0800

    add basic math to get my price for the final pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,22 @@ function Pizza(size, toppings) {
   this.price = 0
 }
 
+Pizza.prototype.finalPrice = function() {
+  this.toppings.forEach(function(topping){
+    this.price += 1;
+  });
+  if (this.size === 1) {
+    this.price += 5;
+  }else if (this.size === 2) {
+    this.price += 7;
+  }else if (this.size === 3) {
+    this.price += 9;
+  }else {
+    alert("Sorry, you need to select a size")
+  }
+  return this.price;
+}
+
 $(document).ready(function(){
   $
 
